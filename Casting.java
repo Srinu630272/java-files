c006d98c2b6b8643506cc5fb6329a79cbff749d0
MV Thanoshan <you@example.com>
2023-07-14 22:33:40 +0530

    commiting type casting and while loop

---
+++ Casting.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Casting {
+    public static void main(String []args){
+        System.out.println("give the input");
+Scanner input=new Scanner(System.in);
+int num = input.nextInt();
+System.out.println(num);
+input.nextLine();
+System.out.println("give the name");
+String name= input.nextLine();
+
+System.out.println(name);
+    }
+    
+}
