405642cd94b335dc0dc7a54bc6ce187378d98d8e
MV Thanoshan <you@example.com>
2023-07-18 11:24:17 +0530

    adding calculator program

--- Calculator_app.java
+++ Calculator_app.java
@@ -2,36 +2,38 @@ import java.util.Scanner;
 public class Calculator_app {
 public static void main(String[]args){
     Scanner sc=new Scanner(System.in);
-    
+    int res=0;
     
     while(true){
-        System.out.println("enter two numbers");
+        System.out.print("enter two numbers ");
         int a=sc.nextInt();
         int b=sc.nextInt();
-        System.out.println("Enter the character");
+        System.out.print("Enter the character ");
         char ch= sc.next().charAt(0);
-        System.out.println("and the result is");
+       
         if(ch=='+'){
-            System.out.println(a+b);
+           res=a+b;
         }
        else if(ch=='-'){
-            System.out.println(a-b);
+            res=a-b;
         }
         else if(ch=='*'){
-            System.out.println(a*b);
+            res=a*b;
         }
         else if(ch=='/'){
-            System.out.println(a/b);
+            res=a/b;
         }
        else if(ch=='%'){
-            System.out.println(a%b);
+           res=a%b;
         }
         else{
             System.out.println("not matched");
         }
 
-
+ System.out.println( "and the result is " + res);
     }
+    
+   
 }
     
 }
